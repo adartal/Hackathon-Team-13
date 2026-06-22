fff4ac3ac63cb206af69b317f8d732807c4eeeb6
EladBlaier <xd.EBlaier@idf.il>
2026-06-23 01:15:18 +0300

    conflict

--- client/src/routeTree.gen.ts
+++ client/src/routeTree.gen.ts
@@ -15,7 +15,6 @@ import { Route as NewRouteImport } from './routes/new'
 import { Route as HomeRouteImport } from './routes/home'
 import { Route as HistoryRouteImport } from './routes/history'
 import { Route as IndexRouteImport } from './routes/index'
-import { Route as StudentsIdRouteImport } from './routes/students.$id'
 import { Route as ReviewIdRouteImport } from './routes/review.$id'
 
 const TeacherHomeRoute = TeacherHomeRouteImport.update({
@@ -48,11 +47,6 @@ const IndexRoute = IndexRouteImport.update({
   path: '/',
   getParentRoute: () => rootRouteImport,
 } as any)
-const StudentsIdRoute = StudentsIdRouteImport.update({
-  id: '/students/$id',
-  path: '/students/$id',
-  getParentRoute: () => rootRouteImport,
-} as any)
 const ReviewIdRoute = ReviewIdRouteImport.update({
   id: '/review/$id',
   path: '/review/$id',
@@ -67,7 +61,6 @@ export interface FileRoutesByFullPath {
   '/teacher': typeof TeacherRoute
   '/teacherHome': typeof TeacherHomeRoute
   '/review/$id': typeof ReviewIdRoute
-  '/students/$id': typeof StudentsIdRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
@@ -77,7 +70,6 @@ export interface FileRoutesByTo {
   '/teacher': typeof TeacherRoute
   '/teacherHome': typeof TeacherHomeRoute
   '/review/$id': typeof ReviewIdRoute
-  '/students/$id': typeof StudentsIdRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
@@ -88,7 +80,6 @@ export interface FileRoutesById {
   '/teacher': typeof TeacherRoute
   '/teacherHome': typeof TeacherHomeRoute
   '/review/$id': typeof ReviewIdRoute
-  '/students/$id': typeof StudentsIdRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
@@ -100,7 +91,6 @@ export interface FileRouteTypes {
     | '/teacher'
     | '/teacherHome'
     | '/review/$id'
-    | '/students/$id'
   fileRoutesByTo: FileRoutesByTo
   to:
     | '/'
@@ -110,7 +100,6 @@ export interface FileRouteTypes {
     | '/teacher'
     | '/teacherHome'
     | '/review/$id'
-    | '/students/$id'
   id:
     | '__root__'
     | '/'
@@ -120,7 +109,6 @@ export interface FileRouteTypes {
     | '/teacher'
     | '/teacherHome'
     | '/review/$id'
-    | '/students/$id'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
@@ -131,7 +119,6 @@ export interface RootRouteChildren {
   TeacherRoute: typeof TeacherRoute
   TeacherHomeRoute: typeof TeacherHomeRoute
   ReviewIdRoute: typeof ReviewIdRoute
-  StudentsIdRoute: typeof StudentsIdRoute
 }
 
 declare module '@tanstack/react-router' {
@@ -178,13 +165,6 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexRouteImport
       parentRoute: typeof rootRouteImport
     }
-    '/students/$id': {
-      id: '/students/$id'
-      path: '/students/$id'
-      fullPath: '/students/$id'
-      preLoaderRoute: typeof StudentsIdRouteImport
-      parentRoute: typeof rootRouteImport
-    }
     '/review/$id': {
       id: '/review/$id'
       path: '/review/$id'
@@ -203,7 +183,6 @@ const rootRouteChildren: RootRouteChildren = {
   TeacherRoute: TeacherRoute,
   TeacherHomeRoute: TeacherHomeRoute,
   ReviewIdRoute: ReviewIdRoute,
-  StudentsIdRoute: StudentsIdRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
